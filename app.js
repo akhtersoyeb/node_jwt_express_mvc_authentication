66a0e97818cfe76aefcedcd1455e6681bdd08b39
akhtersoyeb <saleramestry@gmail.com>
2020-10-26 01:11:25 +0530

    completed tutorial from the net ninja

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const env = require('dotenv');
+const { requireAuth, checkUser } = require('./middleWare/authMiddleware');
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+env.config();
 
 // view engine
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+const dbURI = process.env.MONGODB_URL;
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
-// routes
+// import the routes
+const authRouter = require('./routes/authRouter');
+
+// use routes
+app.get('*', checkUser);
 app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
+app.use(authRouter);
